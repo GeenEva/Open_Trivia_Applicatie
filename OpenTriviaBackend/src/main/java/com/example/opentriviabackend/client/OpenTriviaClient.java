4f4c3be20dde299d0b9b7afc1c8591b577f1e262
geeneva <evavangeenen@gmail.com>
2026-04-15 09:39:59 +0200

    aanpassing voor observability

--- OpenTriviaBackend/src/main/java/com/example/opentriviabackend/client/OpenTriviaClient.java
+++ OpenTriviaBackend/src/main/java/com/example/opentriviabackend/client/OpenTriviaClient.java
@@ -3,6 +3,8 @@ package com.example.opentriviabackend.client;
 import com.example.opentriviabackend.dto.clientResponse.OpenTriviaResponseDto;
 import com.example.opentriviabackend.mapper.TriviaMapper;
 import com.example.opentriviabackend.model.TriviaQuestion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Component
 public class OpenTriviaClient {
+
+    private static final Logger log = LoggerFactory.getLogger(OpenTriviaClient.class);
 
     private final RestTemplate restTemplate;
     private final TriviaMapper mapper;
@@ -26,8 +30,18 @@ public class OpenTriviaClient {
         var response =
                 restTemplate.getForObject(URL, OpenTriviaResponseDto.class);
 
-        if (response == null || response.results() == null ||
-                response.results().isEmpty()) {
+        if (response == null ) {
+            log.warn("OpenTrivia API gaf geen response");
+            return List.of();
+        }
+
+        if (response.responseCode() != 0) {
+            log.warn("OpenTrivia API returned error code: {}", response.responseCode());
+            throw new IllegalStateException("Trivia vragen konden niet goed worden opgehaald.");
+        }
+
+        if ( response.results() == null || response.results().isEmpty()) {
+            log.warn("OpenTrivia API gaf geen vragen terug");
             return List.of();
         }
 
